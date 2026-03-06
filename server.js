c0cc4138ed06c0ae3a9089339e46df1687402e0a
Nithan10 <nithans.21msc@kongu.edu>
2026-03-06 17:54:26 +0530

    secrets removed

--- server.js
+++ server.js
@@ -87,4 +87,4 @@ app.post('/api/login', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
-});
+});      
